665add04291c9ab66a0303b3633722ff3935d46d
genevieve latimer and patricia hall <patricialynnhall@gmail.com>
2016-02-22 20:35:07 -0800

    set conditions and test with html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 $(document).ready(function() {
-  var leapYear = function(year); {
-    if (year % 100 === 0) {
-      return false;
-    } else if (year % 4 === 0) {
-      return true;
-    } else {
-      return false;
-  }
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
 
-  var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-      return true;
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
     } else {
-      return false;
+      $(".not").text("");
     }
-  }
 
-  }
+    $("#result").show();
+    event.preventDefault();
+  });
 });
